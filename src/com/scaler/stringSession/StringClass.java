3c3e4fef72d9b9fce4294d692d49f70520181550
abhay.adgurwar@gmail.com <Abhay@1306>
2022-07-04 09:45:52 +0530

    String Folder added

---
+++ src/com/scaler/stringSession/StringClass.java
@@ -0,0 +1,9 @@
+package com.scaler.stringSession;
+
+public class StringClass {
+    public static void main(String[] args) {
+        String str= "Abhay";
+
+        System.out.println(str);
+    }
+}
